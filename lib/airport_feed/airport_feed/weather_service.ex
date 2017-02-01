07bc10e919ee965e76121a725e06d72055a6b6a7
Matt Engledowl <mengledowl@gmail.com>
2017-02-01 16:26:41 -0600

    Add code to build map based on XML

--- lib/airport_feed/airport_feed/weather_service.ex
+++ lib/airport_feed/airport_feed/weather_service.ex
@@ -3,6 +3,10 @@ defmodule AirportFeed.WeatherService do
   require Logger
   
   @service_url Application.get_env(:airport_feed, :service_url)
+  @weather_attributes ~w(
+    location station_id latitude longitude observation_time_rfc822 weather temperature_string temp_f relative_humidity 
+    wind_string wind_dir wind_degrees
+  )
   
   def fetch(location) do
     weather_url(location)
@@ -20,10 +24,21 @@ defmodule AirportFeed.WeatherService do
     body
     |> :binary.bin_to_list
     |> :xmerl_scan.string
-    |> XML.get_text('/current_observation/weather')
+    |> build_map(@weather_attributes)
   end
   
   defp handle_response({_, %{status_code: status, body: body}}) do
     Logger.error "Error #{status} returned"
+  end
+  
+  defp build_map(document, attrs), do: _build_map(%{}, attrs, document)
+  
+  defp _build_map(map, [], _), do: map
+  defp _build_map(map, [head | tail], document) do
+    head_atom = String.to_atom(head)
+    head_charlist = String.to_charlist(head)
+    
+    Map.put(map, head_atom, XML.get_text(document, '/current_observation/#{head_charlist}'))
+    |> _build_map(tail, document)
   end
 end
--- lib/airport_feed/airport_feed/xml.ex
+++ lib/airport_feed/airport_feed/xml.ex
@@ -1,10 +1,13 @@
 defmodule XML do
   import Record, only: [defrecord: 2, extract: 2]
+  require Logger
   
   defrecord :xmlElement, extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
   defrecord :xmlText, extract(:xmlText, from_lib: "xmerl/include/xmerl.hrl")
   
   def get_text({document, _}, xpath) do
+    Logger.info "xpath: #{xpath}"
+    
     [path_string] = :xmerl_xpath.string(xpath, document)
     [content] = xmlElement(path_string, :content)
     xmlText(content, :value)
